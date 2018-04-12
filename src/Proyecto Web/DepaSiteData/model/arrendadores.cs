135afb257ea6199edac8f2858757e1c319966b4b
darakaiba1 <fate-javier@hotmail.com>
2018-04-12 08:51:48 -0500

    se crearon carpetas nuevas para organizar proyectos

---
+++ src/Proyecto Web/DepaSiteData/model/arrendadores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepaSiteData.model
+{
+    class arrendadores
+    {
+        public int usuarios { get; set; }
+        public int departamentos { get; set; }
+
+    }
+}
